ddeb70db84b75698ac7203b985fcf3156b31172a
ayushg198 <151770512+ayushg198@users.noreply.github.com>
2024-07-05 20:52:12 +0530

    create pages and routes as well as components

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className='text-red-500'>Hello world</div>
+    <div className='text-red-500'>Home Page</div>
   )
 }
---
+++ src/app/posts/[id]/page.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Postpage() {
+  return (
+    <div>Postpage</div>
+  )
+}
